5ae3ed4e3527bfe5f2b149053d25ac07d124123d
YannSko <101813005+YannSko@users.noreply.github.com>
2023-06-14 15:10:12 +0200

    API DONE
    issue to connect on the localsthost (parameter hostname issue on create_bdd.php)

---
+++ Server/api.php
@@ -0,0 +1,20 @@
+<?php
+$con = mysqli_connect("loaclhost","root","tower_defense");
+$response = array();
+if ($con){
+    $sql = "SHOW TABLES";
+    $result = mysqli_query($con, $sql);
+    if ($result){
+        header("Content-Type: application/json");
+        $i = 0;
+        while ($row = mysqli_fetch_array($result)){
+            $response[$i] = $row[0];
+            $i++;
+        }
+        echo json_encode($response, JSON_PRETTY_PRINT);
+    }
+}
+else{
+    echo " Connection a la databse ratée";
+}
+?>
