64ba1e2ac25814c884c7aea59edc99ea022f8c34
YannSko <101813005+YannSko@users.noreply.github.com>
2023-06-14 22:26:51 +0200

    Try to handle the issue of data
    Didnt find the way to fix ( i allowed all memory with the -1 ) and hand time limit. change the forme to prep before query  , but cant handle the generation of data

--- server/generation/generate_data.php
+++ server/generation/generate_data.php
@@ -4,6 +4,9 @@ $hostname = "localhost"; // Remplacez par l'hôte de votre serveur MySQL
 $username = "root"; // Remplacez par votre nom d'utilisateur MySQL
 $password = "root"; // Remplacez par votre mot de passe MySQL
 $database = "tower_defense"; 
+$limit = ini_get('memory_limit');
+ini_set('memory_limit', -1);
+set_time_limit(300); 
 
 //connexion à la base de données
 $connexion = mysqli_connect($hostname, $username, $password, $database);
@@ -188,14 +191,17 @@ function generateRandomDiscussionHistoriqueData($discussionID)
         "ID_historique" => $historiqueID
     ];
 }
+
 //________________générer data + insert dans tableau_________
 // nombre de row à insérer
-$numRows = 2; 
+$numRows = 3;
 for ($i = 0; $i < $numRows; $i++) {
-    //  la table "joueur"
+    // la table "joueur"
     $joueurData = generateRandomJoueurData();
-    $sql_insert_joueur = "INSERT INTO joueur (NameTag, mdp, email, tel, photo_profil) VALUES ('".$joueurData['nametag']."', '".$joueurData['mdp']."', '".$joueurData['email']."', '".$joueurData['tel']."', '".$joueurData['photo_profil']."')";
-    if (mysqli_query($connexion, $sql_insert_joueur)) {
+    $sql_insert_joueur = "INSERT INTO joueur (NameTag, mdp, email, tel, photo_profil) VALUES (?, ?, ?, ?, ?)";
+    $stmt_joueur = mysqli_prepare($connexion, $sql_insert_joueur);
+    mysqli_stmt_bind_param($stmt_joueur, "sssss", $joueurData['nametag'], $joueurData['mdp'], $joueurData['email'], $joueurData['tel'], $joueurData['photo_profil']);
+    if (mysqli_stmt_execute($stmt_joueur)) {
         echo "Données insérées avec succès dans la table 'joueur' !\n";
     } else {
         echo "Erreur lors de l'insertion des données dans la table 'joueur' : " . mysqli_error($connexion);
@@ -203,19 +209,23 @@ for ($i = 0; $i < $numRows; $i++) {
 
     $joueurID = mysqli_insert_id($connexion);
 
-    //  la table "statistiques_joueur"
+    // la table "statistiques_joueur"
     $statistiquesJoueurData = generateRandomStatistiquesJoueurData($joueurID);
-    $sql_insert_statistiques_joueur = "INSERT INTO statistiques_joueur (ID_joueur, parties_jouees_total, nombre_vagues_totales, nombre_vague_maximale, nombre_monstre_total, monstres_tues_individuel, temps_total_jeu, tours_construites_total, tours_individuel_total, tune_totale, score) VALUES ('".$statistiquesJoueurData['ID_joueur']."', '".$statistiquesJoueurData['parties_jouees_total']."', '".$statistiquesJoueurData['nombre_vagues_totales']."', '".$statistiquesJoueurData['nombre_vague_maximale']."', '".$statistiquesJoueurData['nombre_monstre_total']."', '".$statistiquesJoueurData['monstres_tues_individuel']."', '".$statistiquesJoueurData['temps_total_jeu']."', '".$statistiquesJoueurData['tours_construites_total']."', '".$statistiquesJoueurData['tours_individuel_total']."', '".$statistiquesJoueurData['tune_totale']."', '".$statistiquesJoueurData['score']."')";
-    if (mysqli_query($connexion, $sql_insert_statistiques_joueur)) {
+    $sql_insert_statistiques_joueur = "INSERT INTO statistiques_joueur (ID_joueur, parties_jouees_total, nombre_vagues_totales, nombre_vague_maximale, nombre_monstre_total, monstres_tues_individuel, temps_total_jeu, tours_construites_total, tours_individuel_total, tune_totale, score) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $stmt_statistiques_joueur = mysqli_prepare($connexion, $sql_insert_statistiques_joueur);
+    mysqli_stmt_bind_param($stmt_statistiques_joueur, "iiiiiiiiiii", $statistiquesJoueurData['ID_joueur'], $statistiquesJoueurData['parties_jouees_total'], $statistiquesJoueurData['nombre_vagues_totales'], $statistiquesJoueurData['nombre_vague_maximale'], $statistiquesJoueurData['nombre_monstre_total'], $statistiquesJoueurData['monstres_tues_individuel'], $statistiquesJoueurData['temps_total_jeu'], $statistiquesJoueurData['tours_construites_total'], $statistiquesJoueurData['tours_individuel_total'], $statistiquesJoueurData['tune_totale'], $statistiquesJoueurData['score']);
+    if (mysqli_stmt_execute($stmt_statistiques_joueur)) {
         echo "Données insérées avec succès dans la table 'statistiques_joueur' !\n";
     } else {
         echo "Erreur lors de l'insertion des données dans la table 'statistiques_joueur' : " . mysqli_error($connexion);
     }
 
-    //la table "partie_lambda"
+    // la table "partie_lambda"
     $partieLambdaData = generateRandomPartieLambdaData($joueurID);
-    $sql_insert_partie_lambda = "INSERT INTO partie_lambda (ID_joueur, parties_jouees, nombre_vagues_totales, nombre_monstre_total, monstres_tues_individuel, temps_total_partie, tours_construites_total, tours_individuel_total, tune_totale, score) VALUES ('".$partieLambdaData['ID_joueur']."', '".$partieLambdaData['parties_jouees']."', '".$partieLambdaData['nombre_vagues_totales']."', '".$partieLambdaData['nombre_monstre_total']."', '".$partieLambdaData['monstres_tues_individuel']."', '".$partieLambdaData['temps_total_partie']."', '".$partieLambdaData['tours_construites_total']."', '".$partieLambdaData['tours_individuel_total']."', '".$partieLambdaData['tune_totale']."', '".$partieLambdaData['score']."')";
-    if (mysqli_query($connexion, $sql_insert_partie_lambda)) {
+    $sql_insert_partie_lambda = "INSERT INTO partie_lambda (ID_joueur, parties_jouees, nombre_vagues_totales, nombre_monstre_total, monstres_tues_individuel, temps_total_partie, tours_construites_total, tours_individuel_total, tune_totale, score) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $stmt_partie_lambda = mysqli_prepare($connexion, $sql_insert_partie_lambda);
+    mysqli_stmt_bind_param($stmt_partie_lambda, "iiiiiiiiii", $partieLambdaData['ID_joueur'], $partieLambdaData['parties_jouees'], $partieLambdaData['nombre_vagues_totales'], $partieLambdaData['nombre_monstre_total'], $partieLambdaData['monstres_tues_individuel'], $partieLambdaData['temps_total_partie'], $partieLambdaData['tours_construites_total'], $partieLambdaData['tours_individuel_total'], $partieLambdaData['tune_totale'], $partieLambdaData['score']);
+    if (mysqli_stmt_execute($stmt_partie_lambda)) {
         echo "Données insérées avec succès dans la table 'partie_lambda' !\n";
     } else {
         echo "Erreur lors de l'insertion des données dans la table 'partie_lambda' : " . mysqli_error($connexion);
@@ -225,8 +235,10 @@ for ($i = 0; $i < $numRows; $i++) {
 
     // la table "historique_parties"
     $historiquePartiesData = generateRandomHistoriquePartiesData($partieID, $joueurID);
-    $sql_insert_historique_parties = "INSERT INTO historique_parties (ID_historique, ID_partie, ID_joueur, nombre_vagues_totales, nombre_monstre_total, monstres_tues_individuel, temps_total_partie, tours_construites_total, tours_individuel_total, tune_totale, score) VALUES ('".$historiquePartiesData['ID_historique']."', '".$historiquePartiesData['ID_partie']."', '".$historiquePartiesData['ID_joueur']."', '".$historiquePartiesData['nombre_vagues_totales']."', '".$historiquePartiesData['nombre_monstre_total']."', '".$historiquePartiesData['monstres_tues_individuel']."', '".$historiquePartiesData['temps_total_partie']."', '".$historiquePartiesData['tours_construites_total']."', '".$historiquePartiesData['tours_individuel_total']."', '".$historiquePartiesData['tune_totale']."', '".$historiquePartiesData['score']."')";
-    if (mysqli_query($connexion, $sql_insert_historique_parties)) {
+    $sql_insert_historique_parties = "INSERT INTO historique_parties (ID_historique, ID_partie, ID_joueur, nombre_vagues_totales, nombre_monstre_total, monstres_tues_individuel, temps_total_partie, tours_construites_total, tours_individuel_total, tune_totale, score) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $stmt_historique_parties = mysqli_prepare($connexion, $sql_insert_historique_parties);
+    mysqli_stmt_bind_param($stmt_historique_parties, "iiiiiiiiiii", $historiquePartiesData['ID_historique'], $historiquePartiesData['ID_partie'], $historiquePartiesData['ID_joueur'], $historiquePartiesData['nombre_vagues_totales'], $historiquePartiesData['nombre_monstre_total'], $historiquePartiesData['monstres_tues_individuel'], $historiquePartiesData['temps_total_partie'], $historiquePartiesData['tours_construites_total'], $historiquePartiesData['tours_individuel_total'], $historiquePartiesData['tune_totale'], $historiquePartiesData['score']);
+    if (mysqli_stmt_execute($stmt_historique_parties)) {
         echo "Données insérées avec succès dans la table 'historique_parties' !\n";
     } else {
         echo "Erreur lors de l'insertion des données dans la table 'historique_parties' : " . mysqli_error($connexion);
@@ -234,8 +246,10 @@ for ($i = 0; $i < $numRows; $i++) {
 
     // table "post"
     $postData = generateRandomPostData($joueurID);
-    $sql_insert_post = "INSERT INTO post (ID_joueur, Post, Commentaire, Titre, Timestamp) VALUES ('".$postData['ID_joueur']."', '".$postData['Post']."', '".$postData['Commentaire']."', '".$postData['Titre']."', '".$postData['Timestamp']."')";
-    if (mysqli_query($connexion, $sql_insert_post)) {
+    $sql_insert_post = "INSERT INTO post (ID_joueur, Post, Commentaire, Titre, Timestamp) VALUES (?, ?, ?, ?, ?)";
+    $stmt_post = mysqli_prepare($connexion, $sql_insert_post);
+    mysqli_stmt_bind_param($stmt_post, "issss", $postData['ID_joueur'], $postData['Post'], $postData['Commentaire'], $postData['Titre'], $postData['Timestamp']);
+    if (mysqli_stmt_execute($stmt_post)) {
         echo "Données insérées avec succès dans la table 'post' !\n";
     } else {
         echo "Erreur lors de l'insertion des données dans la table 'post' : " . mysqli_error($connexion);
@@ -245,56 +259,48 @@ for ($i = 0; $i < $numRows; $i++) {
 
     //  table "commentaire"
     $commentaireData = generateRandomCommentaireData($joueurID, $postID);
-    $sql_insert_commentaire = "INSERT INTO commentaire (ID_joueur, ID_post, Commentaire, Timestamp) VALUES ('".$commentaireData['ID_joueur']."', '".$commentaireData['ID_post']."', '".$commentaireData['Commentaire']."', '".$commentaireData['Timestamp']."')";
-    if (mysqli_query($connexion, $sql_insert_commentaire)) {
+    $sql_insert_commentaire = "INSERT INTO commentaire (ID_joueur, ID_post, Commentaire, Timestamp) VALUES (?, ?, ?, ?)";
+    $stmt_commentaire = mysqli_prepare($connexion, $sql_insert_commentaire);
+    mysqli_stmt_bind_param($stmt_commentaire, "iiss", $commentaireData['ID_joueur'], $commentaireData['ID_post'], $commentaireData['Commentaire'], $commentaireData['Timestamp']);
+    if (mysqli_stmt_execute($stmt_commentaire)) {
         echo "Données insérées avec succès dans la table 'commentaire' !\n";
     } else {
         echo "Erreur lors de l'insertion des données dans la table 'commentaire' : " . mysqli_error($connexion);
     }
     //  table "message"
     $messageData = generateRandomMessageData($joueurID);
+    $sql_insert_message = "INSERT INTO _message (ID_message, timestamp, ID_joueur, contenu) SELECT ?, ?, ?, ? FROM joueur WHERE ID_joueur = ?";                  
+    $stmt_message = mysqli_prepare($connexion, $sql_insert_message);
+    mysqli_stmt_bind_param($stmt_message, "issis", $messageData['ID_message'], $messageData['timestamp'], $messageData['ID_joueur'], $messageData['contenu'], $messageData['ID_joueur']);
+    if (mysqli_stmt_execute($stmt_message)) {
+        echo "Données insérées avec succès dans la table 'message' !\n";
+    } else {
+        echo "Erreur lors de l'insertion des données dans la table 'message' : " . mysqli_error($connexion);
+    }
 
-$sql_insert_message = "INSERT INTO _message (ID_message, timestamp, ID_joueur, contenu) 
-                       SELECT '".$messageData['ID_message']."', '".$messageData['timestamp']."', '".$messageData['ID_joueur']."', '".$messageData['contenu']."'
-                       FROM joueur
-                       WHERE ID_joueur = '".$messageData['ID_joueur']."'";
-                      
-if (mysqli_query($connexion, $sql_insert_message)) {
-    echo "Données insérées avec succès dans la table 'message' !\n";
-} else {
-    echo "Erreur lors de l'insertion des données dans la table 'message' : " . mysqli_error($connexion);
-}
+    // la table "discussion_mp"
+    $discussionMPData = generateRandomDiscussionMPData($joueurID, $joueurID + 1);
+    $sql_insert_discussion_mp = "INSERT INTO discussion_mp (ID_joueur1, ID_joueur2, ID_discussion, ID_message) SELECT ?, ?, ?, ? FROM joueur WHERE ID_joueur = ? AND EXISTS (SELECT ID_joueur FROM joueur WHERE ID_joueur = ?)";
+    $stmt_discussion_mp = mysqli_prepare($connexion, $sql_insert_discussion_mp);
+    mysqli_stmt_bind_param($stmt_discussion_mp, "iiiiii", $discussionMPData['ID_joueur1'], $discussionMPData['ID_joueur2'], $discussionMPData['ID_discussion'], $discussionMPData['ID_message'], $discussionMPData['ID_joueur1'], $discussionMPData['ID_joueur2']);
+    if (mysqli_stmt_execute($stmt_discussion_mp)) {
+        echo "Données insérées avec succès dans la table 'discussion_mp' !\n";
+    } else {
+        echo "Erreur lors de l'insertion des données dans la table 'discussion_mp' : " . mysqli_error($connexion);
+    }
 
-// la table "discussion_mp"
-$discussionMPData = generateRandomDiscussionMPData($joueurID, $joueurID + 1);
-
-$sql_insert_discussion_mp = "INSERT INTO discussion_mp (ID_joueur1, ID_joueur2, ID_discussion, ID_message) 
-                            SELECT '".$discussionMPData['ID_joueur1']."', '".$discussionMPData['ID_joueur2']."', '".$discussionMPData['ID_discussion']."', '".$discussionMPData['ID_message']."'
-                            FROM joueur
-                            WHERE ID_joueur = '".$discussionMPData['ID_joueur1']."'
-                            AND EXISTS (SELECT ID_joueur FROM joueur WHERE ID_joueur = '".$discussionMPData['ID_joueur2']."')";
-                            
-if (mysqli_query($connexion, $sql_insert_discussion_mp)) {
-    echo "Données insérées avec succès dans la table 'discussion_mp' !\n";
-} else {
-    echo "Erreur lors de l'insertion des données dans la table 'discussion_mp' : " . mysqli_error($connexion);
-}
-
-//  table "discussion_historique"
-$discussionHistoriqueData = generateRandomDiscussionHistoriqueData($discussionMPData['ID_discussion']);
-
-$sql_insert_discussion_historique = "INSERT INTO discussion_historique (ID_discussion, ID_historique) 
-                                    SELECT '".$discussionHistoriqueData['ID_discussion']."', '".$discussionHistoriqueData['ID_historique']."'
-                                    FROM discussion_mp
-                                    WHERE ID_discussion = '".$discussionHistoriqueData['ID_discussion']."'";
-
-if (mysqli_query($connexion, $sql_insert_discussion_historique)) {
+    //  table "discussion_historique"
+    $discussionHistoriqueData = generateRandomDiscussionHistoriqueData($discussionMPData['ID_discussion']);
+$sql_insert_discussion_historique = "INSERT INTO discussion_historique (ID_discussion, ID_historique) SELECT ?, ? FROM discussion_mp WHERE ID_discussion = ?";
+$stmt_discussion_historique = mysqli_prepare($connexion, $sql_insert_discussion_historique);
+mysqli_stmt_bind_param($stmt_discussion_historique, "ssi", $discussionHistoriqueData['ID_discussion'], $discussionHistoriqueData['ID_historique'], $discussionHistoriqueData['ID_discussion']);
+if (mysqli_stmt_execute($stmt_discussion_historique)) {
     echo "Données insérées avec succès dans la table 'discussion_historique' !\n";
 } else {
     echo "Erreur lors de l'insertion des données dans la table 'discussion_historique' : " . mysqli_error($connexion);
 }
-
 }
 mysqli_close($connexion);
+ini_set('memory_limit', $limit);
 ?>
 
