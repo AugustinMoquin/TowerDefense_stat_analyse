98eac78a1e3b758823a8de0bf9fe9427e1186272
YannSko <101813005+YannSko@users.noreply.github.com>
2023-06-20 18:39:29 +0200

    MAJ

---
+++ client/pages/Illustration/general/partie_lambda.php
@@ -0,0 +1,159 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "tower_defense";
+
+$con = mysqli_connect($hostname, $username, $password, $database);
+
+if (!$con) {
+    die("Connexion à la base de données échouée : " . mysqli_connect_error());
+}
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $ID_user = $_GET['ID_user'];
+    $ID_partie = $_GET['ID_partie'];
+
+    // recup id spe user partie
+    $sql = "SELECT * FROM partie_lambda WHERE ID_partie = $ID_partie AND ID_user = $ID_user";
+    $result = mysqli_query($con, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $partie_lambda_stats = mysqli_fetch_assoc($result);
+
+        // determine win ou loose
+        $partieWin = ($partie_lambda_stats["win"] == 0) ? "Victoire" : "Défaite";
+
+        //recup la moyenne
+        $sql = "SELECT AVG(nombre_vagues_totales) AS avg_vagues, AVG(nombre_monstre_total) AS avg_monstres, AVG(monstres_tues_individuel) AS avg_monstres_tues, AVG(temps_total_partie) AS avg_temps, AVG(tours_construites_total) AS avg_tours_construites, AVG(tours_individuel_total) AS avg_tours_individuel, AVG(tune_totale) AS avg_tune, AVG(score) AS avg_score, AVG(duree_jeu) AS avg_duree_jeu FROM partie_lambda WHERE ID_user = $ID_user";
+        $result = mysqli_query($con, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            $average_lambda_stats = mysqli_fetch_assoc($result);
+
+            // affich stat et chart
+            echo '<!DOCTYPE HTML>
+            <html>
+            <head>
+                <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                <style>
+                    .container {
+                        max-width: 600px;
+                        margin: 0 auto;
+                        padding: 20px;
+                    }
+            
+                    h1 {
+                        text-align: center;
+                    }
+            
+                    form {
+                        display: flex;
+                        flex-direction: column;
+                        margin-bottom: 20px;
+                    }
+            
+                    form label {
+                        font-weight: bold;
+                    }
+            
+                    form input {
+                        margin-bottom: 10px;
+                    }
+                </style>
+                <script>
+                // Function to general_historique.php
+                function goBack() {
+                    window.location.href = "general_historique.php";
+                }
+                // Functione to stat_player.php
+                function goToStatPlayer() {
+            var userID = ' . $ID_user . ';
+            window.location.href = "stat_player.php?ID_user=" + userID;
+        }
+            </script>
+            </head>
+            <body>
+            <div class="container">
+            <h2>ID Utilisateur: <a href="javascript:void(0)" onclick="goToStatPlayer()">' . $ID_user . '</a></h2>
+                <h1>' . $partieWin . '</h1>
+                <canvas id="myChart"></canvas>
+                
+            <button class="back-button" onclick="goBack()">Retour</button>
+
+   
+                <script>
+                    // Recup PHP variables hop dans JavaScript variables
+                    var partieLambdaStats = ' . json_encode($partie_lambda_stats) . ';
+                    var averageLambdaStats = ' . json_encode($average_lambda_stats) . ';
+            
+                    //BARCHART
+                    var labels = ["Vagues", "Monstres", "Monstres tués", "Temps", "Tours construites", "Tours individuelles", "Tune", "Score", "Durée du jeu"];
+                    var dataPartieLambda = [
+                        partieLambdaStats.nombre_vagues_totales,
+                        partieLambdaStats.nombre_monstre_total,
+                        partieLambdaStats.monstres_tues_individuel,
+                        partieLambdaStats.temps_total_partie,
+                        partieLambdaStats.tours_construites_total,
+                        partieLambdaStats.tours_individuel_total,
+                        partieLambdaStats.tune_totale,
+                        partieLambdaStats.score,
+                        partieLambdaStats.duree_jeu
+                    ];
+                    var dataAverageLambda = [
+                        averageLambdaStats.avg_vagues,
+                        averageLambdaStats.avg_monstres,
+                        averageLambdaStats.avg_monstres_tues,
+                        averageLambdaStats.avg_temps,
+                        averageLambdaStats.avg_tours_construites,
+                        averageLambdaStats.avg_tours_individuel,
+                        averageLambdaStats.avg_tune,
+                        averageLambdaStats.avg_score,
+                        averageLambdaStats.avg_duree_jeu
+                    ];
+            
+                    // chart
+                    var ctx = document.getElementById("myChart").getContext("2d");
+                    var chart = new Chart(ctx, {
+                        type: "bar",
+                        data: {
+                            labels: labels,
+                            datasets: [{
+                                label: "Partie Lambda",
+                                data: dataPartieLambda,
+                                backgroundColor: "rgba(54, 162, 235, 0.5)",
+                                borderColor: "rgba(54, 162, 235, 1)",
+                                borderWidth: 1
+                            },
+                            {
+                                label: "Moyenne Lambda",
+                                data: dataAverageLambda,
+                                backgroundColor: "rgba(255, 99, 132, 0.5)",
+                                borderColor: "rgba(255, 99, 132, 1)",
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                                }
+                            }
+                        }
+                    });
+                </script>
+            </div>
+            </body>
+            </html>';
+        } else {
+            echo "Aucune donnée trouvée pour les statistiques moyennes.";
+        }
+    } else {
+        echo "Aucune donnée trouvée pour cette partie.";
+    }
+} else {
+    echo "Mauvaise méthode de requête.";
+}
+mysqli_close($con);
+?>
---
+++ client/pages/Illustration/general/stat_player.php
@@ -0,0 +1,163 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "tower_defense"; 
+
+
+$con = mysqli_connect($hostname, $username, $password, $database);
+
+
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+$user_id = isset($_GET['ID_user']) ? $_GET['ID_user'] : 0; //recup depuis l'url via get
+
+
+
+// Requête s données user
+$query = "SELECT 
+    AVG(parties_jouees) AS avg_parties_jouees,
+    AVG(nombre_vagues_totales) AS avg_nombre_vagues_totales,
+    AVG(nombre_monstre_total) AS avg_nombre_monstre_total,
+    AVG(monstres_tues_individuel) AS avg_monstres_tues_individuel,
+    AVG(temps_total_partie) AS avg_temps_total_partie,
+    AVG(tours_construites_total) AS avg_tours_construites_total,
+    AVG(tours_individuel_total) AS avg_tours_individuel_total,
+    AVG(tune_totale) AS avg_tune_totale,
+    AVG(score) AS avg_score,
+    AVG(duree_jeu) AS avg_duree_jeu
+FROM partie_lambda
+WHERE ID_user = '" . mysqli_real_escape_string($con, $user_id) . "'";
+
+
+$result = mysqli_query($con, $query);
+
+// Récupdonnéesrequête
+$dataRow = mysqli_fetch_assoc($result);
+
+// Donné barchart
+$dataBar = array(
+    array("label" => "Parties Jouées", "y" => $dataRow['avg_parties_jouees']),
+    array("label" => "Nombre de Vagues Totales", "y" => $dataRow['avg_nombre_vagues_totales']),
+    array("label" => "Nombre de Monstres Total", "y" => $dataRow['avg_nombre_monstre_total']),
+    array("label" => "Monstres Tués Individuel", "y" => $dataRow['avg_monstres_tues_individuel']),
+    array("label" => "Temps Total de Partie", "y" => $dataRow['avg_temps_total_partie']),
+    array("label" => "Tours Construites Total", "y" => $dataRow['avg_tours_construites_total']),
+    array("label" => "Tours Individuel Total", "y" => $dataRow['avg_tours_individuel_total']),
+    array("label" => "Tune Totale", "y" => $dataRow['avg_tune_totale']),
+    array("label" => "Score", "y" => $dataRow['avg_score']),
+    array("label" => "Durée de Jeu", "y" => $dataRow['avg_duree_jeu'])
+);
+
+$queryPie = "SELECT win, COUNT(*) AS count FROM partie_lambda WHERE ID_user = '$user_id' GROUP BY win";
+$resultPie = mysqli_query($con, $queryPie);
+
+// Données piechart
+$dataPointsPie = array();
+while ($rowPie = mysqli_fetch_assoc($resultPie)) {
+    $labelPie = $rowPie['win'] == 0 ? 'Win' : 'Loss';
+    $dataPointsPie[] = array(
+        "label" => $labelPie,
+        "y" => $rowPie['count']
+    );
+}
+
+mysqli_close($con);
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+    #userForm {
+        margin-bottom: 10px;
+    }
+</style>
+</head>
+<body>
+
+
+<button onclick="goBack()">Retour aux stats de partie</button>
+    <button onclick="window.location.href='general_historique.php'">Historique G</button>
+
+
+<div id="chartContainerPie" style="height: 370px; width: 100%;"></div>
+<div id="chartContainerBar" style="height: 370px; width: 100%;"></div>
+
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<script>
+    function goBack() {
+        window.history.back();
+    }
+</script>
+<script>
+    //___________________________________________________________PieCHART____________________________________________
+window.onload = function() {
+    var chartPie = new CanvasJS.Chart("chartContainerPie", {
+        theme: "light2",
+        animationEnabled: true,
+        title: {
+            text: "User <?php echo $user_id; ?> Win/Loss Stats"
+        },
+        data: [{
+            type: "pie",
+            indexLabel: "{y}",
+            yValueFormatString: "#,##0",
+            indexLabelPlacement: "inside",
+            indexLabelFontColor: "#36454F",
+            indexLabelFontSize: 18,
+            indexLabelFontWeight: "bolder",
+            showInLegend: true,
+            legendText: "{label}",
+            dataPoints: <?php echo json_encode($dataPointsPie, JSON_NUMERIC_CHECK); ?>
+        }]
+    });
+//___________________________________________________________BARCHART____________________________________________
+    var chartBar = new CanvasJS.Chart("chartContainerBar", {
+        theme: "light2",
+        animationEnabled: true,
+        title: {
+            text: "User <?php echo $user_id; ?> Average Stats"
+        },
+        axisY: {
+            title: "Average Value"
+        },
+        data: [{
+            type: "column",
+            indexLabel: "{y}",
+            yValueFormatString: "#,##0.00",
+            dataPoints: <?php echo json_encode($dataBar, JSON_NUMERIC_CHECK); ?>
+        }]
+    });
+
+    // Calcul total wins/losses
+    var totalWins = 0;
+    var totalLosses = 0;
+    <?php
+    foreach ($dataPointsPie as $dataPointPie) {
+        if ($dataPointPie['label'] == 'Win') {
+            echo 'totalWins += ' . $dataPointPie['y'] . ';';
+        } else {
+            echo 'totalLosses += ' . $dataPointPie['y'] . ';';
+        }
+    }
+    ?>
+
+    // pourcentage de victoire
+    var totalGames = totalWins + totalLosses;
+    var winPercentage = (totalWins / totalGames) * 100;
+    var lossPercentage = (totalLosses / totalGames) * 100;
+
+    // Pourcentage de victoire dans le titre 
+    chartPie.options.title.text += " (WinRate: " + winPercentage.toFixed(2) + "%)";
+
+    chartPie.render();
+    chartBar.render();
+}
+</script>
+
+</body>
+</html>
